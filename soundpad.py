e0aa717ca12d3964ffb2c7722ca1f4b5dd9985a6
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2022-05-02 11:58:15 +0200

    Revert "debug level set from CLI"
    This reverts commit 9b001682cd8cebf8d3e1a927d717808c49801539.

--- soundpad.py
+++ soundpad.py
@@ -1,4 +1,3 @@
-from distutils import log
 import logging
 import sys
 
@@ -6,41 +5,22 @@ __version__ = 3.0
 __author__ = "Adam Walkiewicz"
 github = "https://github.com/ajwalkiewicz/sound-pad"
 
-DEFAULT_LOGGING_LEVEL: logging.ERROR
-
-LOGGING_LEVEL_MAP = {
-    "debug": logging.DEBUG,
-    "info": logging.INFO,
-    "warning": logging.WARNING,
-    "error": logging.ERROR,
-    "critical": logging.CRITICAL
-}
-
-def main():
-    if len(sys.argv) > 1:
-        logging_level = LOGGING_LEVEL_MAP.get(str(sys.argv[1]).lower(), "error")
-    else:
-        logging_level = DEFAULT_LOGGING_LEVEL
-    
-    logging.basicConfig(
+LOGGING_LEVEL = logging.INFO
+logging.basicConfig(
     format="[%(asctime)s][%(levelname)s][%(filename)s:%(funcName)s:%(lineno)d] %(message)s",
-    level=logging_level
+    level=LOGGING_LEVEL
     )
-    print(f"Logging level set to: {logging_level}")
 
-    system = sys.platform
-    if system == "linux":
-        logging.info(f"System detected: {system}")
-        import modules.linux_app as app
-        app.run()
-    elif system == "win32":
-        logging.info(f"System detected: {system}")
-        import modules.win_app as app
-        app.run()
-    else:
-        logging.info(f"System detected: {system}, runing in linux configuration")
-        import modules.linux_app as app
-        app.run()
-
-if __name__ == "__main__":
-    main()
+system = sys.platform
+if system == "linux":
+    logging.info(f"System detected: {system}")
+    import modules.linux_app as app
+    app.run()
+elif system == "win32":
+    logging.info(f"System detected: {system}")
+    import modules.win_app as app
+    app.run()
+else:
+    logging.info(f"System detected: {system}, runing in linux configuration")
+    import modules.linux_app as app
+    app.run()

825585b3e80dfc2baa5ecf9021779fb6864915f6
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2024-06-29 13:15:41 +0200

    Move settings to a separate module
    * Remove utils.py module and move its reponsibilities to new
    settings.py module
    * Create settings.py module containging global settings
    * Add stop_all, play_pause_all and fadeaut_all methods to the
    player

--- modules/utils.py
+++
@@ -1,5 +0,0 @@
-help_message = """
-Simple sound pad inspired by real professional sound pads. 
-It allows to ascribe music file to the button and play it
-by pressing the button or key on keyboard.
-"""

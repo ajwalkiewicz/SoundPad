63c79693298a75e2b7ab4811aa4a72eed3e59f80
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2024-07-12 21:51:30 +0200

    Fix corner cases for settings and playlist
    * Add try except for loading settings from JSON
    * Add try except for loading project from JSON
    * Fix bug that sounds were not stopped when new project
    was loaded
    * Add default path for all sounds that inherits form BaseSound

--- modules/playlist.py
+++ modules/playlist.py
@@ -1,26 +1,26 @@
 import logging
 from collections import UserList
-from typing import List, Union
 
-from modules.audio import BaseSound, NoSound
+from modules.audio import NoSound, Sound
+from modules import settings
 
 
 class Playlist(UserList):
     """Class responsible for storing a playlist"""
 
-    MAX_SOUNDS = 9
-
-    def __init__(self, initlist=None, max_sounds: Union[int, None] = None) -> None:
-        max_sounds = max_sounds or Playlist.MAX_SOUNDS
+    def __init__(
+        self, initlist: list[Sound | NoSound] = None, max_sounds: None | int = None
+    ) -> None:
+        max_sounds = max_sounds or settings.NUMBER_OF_SOUNDS
 
         if initlist is None:
-            initlist: List[BaseSound] = [NoSound() for _ in range(max_sounds)]
+            initlist: list[NoSound] = [NoSound() for _ in range(max_sounds)]
 
         super().__init__(initlist=initlist)
         logging.debug(f"Initialized {self}")
 
     def get_paths(self) -> list[str]:
-        return [sound.path if sound else "" for sound in self]
+        return [sound.path for sound in self]
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}(initlist={self.data}, max_sounds={len(self)})"
